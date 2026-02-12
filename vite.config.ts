92fcebaef0138650583e5a260b05ab98252d3e9f
MatheusFujimura1 <matheusfujimura2003@gmail.com>
2026-02-12 10:07:32 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Define o caminho base como relativo. Isso corrige o erro de tela branca no GitHub Pages.
+  base: './Inventario', // Define o caminho base como relativo. Isso corrige o erro de tela branca no GitHub Pages.
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
